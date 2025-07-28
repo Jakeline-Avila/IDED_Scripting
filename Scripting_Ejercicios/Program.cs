638479866e26543321543fd3b32b65c69483d77b
Jake <Jacke.verbel@gmail.com>
2025-07-28 05:55:26 -0500

    añadiendo ejercicios

---
+++ Scripting_Ejercicios/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Scripting_Ejercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
